7ce92a02cde6f4eb962d271ded4ae54b54e58bad
Shreyash Chougule <shreyashchougule1080@gmail.com>
2025-04-02 16:10:39 +0530

    Added Connection Helper but not using for now

---
+++ Common/DatabaseHelper/SqlServerConnectionHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;   
+
+namespace Common.DatabaseHelper
+{
+    public class SqlServerConnectionHelper : IDatabaseConnectionHelper
+    {
+        private readonly string _connectionString;
+
+        public SqlServerConnectionHelper(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IEnumerable<TReturn>> QueryAsync<TReturn>(string query, object param, CommandType commandType)
+        {
+            IEnumerable<TReturn> rows = null;
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync(); // Open connection first
+                rows = await connection.QueryAsync<TReturn>(query, param, commandType: commandType); // Execute query
+            }
+            return rows;
+        }
+    }
+}
