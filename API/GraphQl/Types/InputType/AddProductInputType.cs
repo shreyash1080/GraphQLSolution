b1f2a227644269b28136304797fedc78a262e2c8
Shreyash Chougule <shreyashchougule1080@gmail.com>
2025-04-02 16:09:47 +0530

    small fix

---
+++ API/GraphQl/Types/InputType/AddProductInputType.cs
@@ -0,0 +1,32 @@
+﻿using API.GraphQl;
+using API.GraphQL;
+using Application.Models;
+using HotChocolate.Types;
+
+namespace API.GraphQl.Types.InputType
+{
+    //✅ InputObjectType<T> tells GraphQL that this is an input structure, not a database entity.
+    public class ProductInputType : InputObjectType<ProductInput>
+    {
+        protected override void Configure(IInputObjectTypeDescriptor<ProductInput> descriptor)
+        {
+            descriptor.Name("ProductInput");
+
+            descriptor.Field(x => x.Name)
+                .Type<NonNullType<StringType>>()
+                .Description("The name of the product.");
+
+            descriptor.Field(x => x.Price)
+                .Type<NonNullType<DecimalType>>()
+                .Description("The price of the product.");
+        }
+    }
+}
+
+
+//Feature                  InputObjectType<T>(Mutation Input)              ObjectType<T>(Query Output)
+//Used for	               Sending data to mutations	                     Returning data in queries
+//Allows resolvers	       ❌ No	                                         ✅ Yes
+//Allows computed fields   ❌ No	                                         ✅ Yes
+//Used in	               Mutation arguments	                             Query & Mutation responses
+//Example	               ProductInputType	                                 ProductType
---
+++ API/GraphQl/Types/OutputType/ProductType.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using System.Xml.Linq;
+
+namespace API.GraphQl.Types.InputType
+{
+    //ProductType is a custom GraphQL type that extends(:) ObjectType<Product>.
+    //It tells GraphQL how to structure the Product entity when exposing it through queries or mutations.
+    public class ProductType : ObjectType<Product> //ObjectType<T> is a built-in class from HotChocolate that helps map C# objects (Product) to GraphQL types.
+    {
+        protected override void Configure(IObjectTypeDescriptor<Product> descriptor)//The Configure method is used to modify how GraphQL exposes the Product entity.
+        {
+            descriptor.Name("Product"); // Sets GraphQL type name to "Product"
+
+            descriptor.Field(x => x.Name).Description("This is Product");
+
+            descriptor.Field(x => x.Id).Description("The unique identifier of the product");
+
+            descriptor.Field(x => x.Price).Description("This is price feild");
+        }
+    }
+}
+
+//What is IObjectTypeDescriptor<Product> ?
+//descriptor is an instance of IObjectTypeDescriptor<Product>, which helps define how GraphQL should treat the Product class.
+//It is used to:
+//Rename fields(if needed).
+//Add descriptions(helps API consumers).
+//Hide fields(if needed).
+//Define relationships with other entities.
